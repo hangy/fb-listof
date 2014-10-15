fa34ffd009e754e9d5a451acd2fd26690d21ddb8
hangy <hangy@hangy.de>
2014-10-15 14:06:34 +0200

    Fixed event handling for showing the table.

--- components/table-selector/table-selector.js
+++ components/table-selector/table-selector.js
@@ -1,5 +1,8 @@
 Polymer('table-selector', {
   ready: function() {
     this.tables = tables;
+  },
+  submitClick: function(event, detail, sender) {
+    this.fire('tableSelected', sender.value);
   }
 });
